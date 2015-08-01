cd83030673d1d468a83c7af7b6db950ed0c47ad8
Tami Wohlers <tamiwohelrs28@yahoo.com>
2015-08-01 14:58:44 -0600

    working well first form

--- patient/patientController.js
+++ patient/patientController.js
@@ -1,5 +1,21 @@
 var app = angular.module('app');
 
 app.controller('patientCtrl', function($scope, patientService){
+ 
+  $scope.mileageCalc = function(miles) {
+		$scope.reimbursement = patientService.mileageCalc(miles);
+	};
+  $scope.patientList = patientService.patientData;
 
-});
+$scope.findPatient = function(ptName){
+      $scope.ptSearch = patientService.findPatient(ptName) 
+  }
+  });
+
+  
+  // $scope.displayPtInfo = function(selection) {
+  // 	console.log('before display ', selection);
+  // 	$scope.ptInfo = patientService.displayPtInfo(selection)
+
+  // }
+    // $scope.currentPerson = patientList[0].person
--- patient/patientService.js
+++ patient/patientService.js
@@ -1,3 +1,44 @@
-app.service('patientService', function(){
-	  
-	});
+app.service('patientService', function() {
+            this.patientData = [
+            {
+                ID: 0,
+                person: 'Wanda Smith',
+                diagnosis: 'lung cancer'
+            }, {
+                ID: 1,
+                person: 'Clyde Brothers',
+                diagnosis: 'AIDS'
+            }, {
+                ID: 2,
+                person: 'Larry Olsen',
+                diagnosis: 'liver failure'
+            }
+            ];
+
+            this.mileageCalc = function(numMiles) {
+                    var reimbursement = numMiles * .50;
+                    return reimbursement;
+                }
+                // this.displayPtInfo = function(selection) {
+                // 	return this.patientData[id];
+                // }
+
+            this.findPatient = function(ptName) {
+            	var found = false;	
+
+                for (var i = 0; i < this.patientData.length; i++) {
+                	found = false;
+                    //console.log("patientData", this.patientData[i])
+                    console.log("ptName", ptName)
+                    if(ptName == this.patientData[i].person) {
+                    	found = true;
+          				return ptName;
+                    }
+
+                    
+               }
+               if(found === false) {
+               	return "pt not found";
+               }
+            }
+        });
